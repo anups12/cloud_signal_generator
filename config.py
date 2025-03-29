fdcc1c11cd9ef67213f4727800fcd805a177037b
anup singh <bossaman102@gmail.com>
2025-03-29 11:49:10 +0530

    Cleanup

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
+
 class Config:
-    SECRET_KEY = '725c906f430c6017dd8a5b75553fb6a0'
-    TARGET_URL = 'https://your-signal-receiver-url.com/signal'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
