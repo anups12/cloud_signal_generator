9ba096e568702c6aea9ce164846666065fea1198
anup singh <bossaman102@gmail.com>
2025-03-30 02:57:26 +0530

    Cleanup

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from flask_socketio import disconnect
 import json
 
 bp = Blueprint('main', __name__)
-dropdown_list = ['NIFTY', 'BANKNIFTY', 'TCS', 'INFY', 'RELIANCE', 'ICICIBANK', 'SBIN']
+dropdown_list =  ['Solana (OTC)', 'FTSE China A50 Index', 'GBP/CAD (OTC)', 'USD/PHP (OTC)', 'Dogwifhat (OTC)', 'Intel (OTC)', 'GBP/JPY (OTC)', 'USD/INR (OTC)', 'Floki (OTC)', 'Pepe (OTC)', 'Microsoft (OTC)', 'NZD/CHF (OTC)', 'Gold (OTC)', 'USD/MXN (OTC)', 'EUR/JPY (OTC)', 'CAD/CHF (OTC)', 'S&P/ASX 200', 'GBP/CHF (OTC)', 'NZD/USD (OTC)', 'Litecoin (OTC)', 'USD/ZAR (OTC)', 'Dow Jones', 'GBP/AUD (OTC)', 'NZD/JPY (OTC)', 'USD/NGN (OTC)', 'FTSE 100', 'CAC 40', 'USD/IDR (OTC)', 'Bitcoin Cash (OTC)', 'Bitcoin (OTC)', 'USD/TRY (OTC)', 'USD/ARS (OTC)', 'AUD/USD (OTC)', 'USD/JPY (OTC)', 'AUD/CAD (OTC)', 'American Express (OTC)', 'GBP/NZD (OTC)', 'Silver (OTC)', 'Hong Kong 50', 'Binance Coin (OTC)', 'EUR/NZD (OTC)', 'Shiba Inu (OTC)', 'Bonk (OTC)', 'USD/BDT (OTC)', 'EUR/CHF (OTC)', 'GBP/USD (OTC)', 'IBEX 35', 'Ethereum (OTC)', 'TRON (OTC)', 'AUD/NZD (OTC)', 'EUR/AUD (OTC)', 'Boeing Company (OTC)', 'USD/CHF (OTC)', 'EUR/SGD (OTC)', 'USD/BRL (OTC)', 'Johnson & Johnson (OTC)', 'USD/EGP (OTC)', 'AUD/JPY (OTC)', 'CHF/JPY (OTC)', 'EUR/CAD (OTC)', 'EURO STOXX 50', 'Pfizer Inc (OTC)', 'Toncoin (OTC)', 'Dogecoin (OTC)', 'USD/PKR (OTC)', 'AUD/CHF (OTC)', 'USD/COP (OTC)', 'NASDAQ 100', 'USD/DZD (OTC)', 'Trump (OTC)', 'EUR/USD (OTC)', 'UKBrent (OTC)', 'USD/CAD (OTC)', 'Ripple (OTC)', "McDonald's (OTC)", 'FACEBOOK INC (OTC)', 'CAD/JPY (OTC)', 'Nikkei 225', 'USCrude (OTC)', 'EUR/GBP (OTC)', 'NZD/CAD (OTC)']
 
 @bp.route('/', methods=['GET'])
 def index():
